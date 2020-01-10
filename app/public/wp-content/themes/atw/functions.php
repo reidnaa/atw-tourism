14b49de2ac858ed07ca21e84d051afa89baa0c2f
reid naaykens <reidnaaykens@gmail.com>
2020-01-09 16:15:06 -0800

    login styling and functions

--- app/public/wp-content/themes/atw/functions.php
+++ app/public/wp-content/themes/atw/functions.php
@@ -11,7 +11,23 @@
 
 require_once dirname(__FILE__) . '/inc/widgets.php';
 
+function admin_styles(){
+    wp_enqueue_style('loginCSS', get_template_directory_uri() . '/login/css/style.css', false);
+    wp_enqueue_style('vegasCSS', get_template_directory_uri() . '/login/css/vegas.min.css', false);
+    
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('loginjs', get_template_directory_uri() . '/login/js/login.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('vegasjs', get_template_directory_uri() . '/login/js/vegas.min.js', array('jquery'), '1.0', true);
 
+    wp_localize_script(
+        'loginjs', 
+        'login_images', 
+        array(
+        'theme_path' => get_template_directory_uri() . '/login/img'
+
+    ));
+}
+add_action('login_enqueue_scripts', 'admin_styles', 10);
 
 function slider(){
     $args = array(
